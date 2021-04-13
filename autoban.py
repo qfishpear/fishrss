95c77d6b4e9815d8790b3177f88b8edcf415186b
fishpear <fish>
2021-04-13 16:57:58 +0200

    add debug option

--- autoban.py
+++ autoban.py
@@ -8,11 +8,17 @@ import time
 import base64
 import json
 import urllib
+import argparse
 from IPython import embed
 from collections import defaultdict
 
 from config import CONFIG
 from common import logger, redapi, flush_logger
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--stats', action='store_true', default=False,
+                    help="show stats of all the uploaders")
+args = parser.parse_args()
 
 DIC_URL = "dicmusic.club"
 RED_URL = "redacted.ch"
@@ -25,7 +31,7 @@ AUTOBAN = CONFIG["red"]["autoban"]
 DELUGE = CONFIG["deluge"]
 client = deluge_client.DelugeRPCClient(DELUGE["ip"], DELUGE["port"], DELUGE["username"], DELUGE["password"])
 client.connect()
-logger.info("is connected: {}".format(client.connected))
+logger.info("autoban: deluge is connected: {}".format(client.connected))
 assert client.connected
 
 tlist = list(client.core.get_torrents_status({}, ["hash", "ratio", "progress", "total_size", "time_added", "comment"]).values())
@@ -49,6 +55,9 @@ for uploader, torrents in uploaders.items():
         total_ul = sum([t[b"total_size"] * t[b"ratio"] * t[b"progress"] / 100 for t in torrents])
         total_size = sum([t[b"total_size"] * t[b"progress"] / 100 for t in torrents])
         ratio = total_ul / total_size
+        if args.stats:
+            logger.info("uploader: {} #torrents: {} ratio: {:.3f} {:.3f}GB/{:.3f}GB".format(
+                uploader, len(torrents), ratio, total_ul/1024**3, total_size/1024**3))
         for cond in AUTOBAN["ratio"]:
             if len(torrents) >= cond["count"] and ratio < cond["ratiolim"]:
                 bannedusers.add(uploader)
