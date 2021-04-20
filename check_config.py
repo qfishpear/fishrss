df49e573b087c307081ea52d348da966fbe0ea05
fishpear <fish>
2021-04-20 09:44:41 +0200

    add more check

--- check_config.py
+++ check_config.py
@@ -1,12 +1,23 @@
+import os
 from common import get_api, get_filter, logger, SITE_CONST
 from config import CONFIG
 import traceback
 
+def check_path(path, name):
+    if path is not None:
+        if not os.path.exists(path):
+            logger.info("{}不存在，请创建:{}".format(name, path))
+
+# 其他
+check_path(CONFIG["filter"]["source_dir"], "source_dir")
+check_path(CONFIG["filter"]["dest_dir"], "dest_dir")
+# 网站
 configured_sites = {}
 for site in SITE_CONST.keys():
-    if site not in CONFIG:
+    if site not in CONFIG.keys():
         logger.info("{}未配置".format(site))
     else:
+        check_path(CONFIG[site]["filter_config"]["banlist"], "{}的banlist".format(site))
         try:
             api = get_api(site)
         except:
@@ -17,3 +28,14 @@ for site in SITE_CONST.keys():
         except:
             logger.info("error in {}'s filter_config".format(site))
             logger.info(traceback.format_exc())
+# bt客户端
+try:
+    import deluge_client
+    DELUGE = CONFIG["deluge"]
+    de = deluge_client.DelugeRPCClient(DELUGE["ip"], DELUGE["port"], DELUGE["username"], DELUGE["password"])
+    de.connect()
+    assert(de.connected)
+    logger.info("deluge is correctly configured")
+except:
+    logger.info("deluge配置错误无法连接")
+    logger.info(traceback.format_exc())
